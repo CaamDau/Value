d68c992b0c4cfe9a3395e39c49f392041fb18151
LCD <565726319@qq.com>
2020-04-25 11:53:49 +0800

    🔧 LCD -

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,23 @@
+//Created  on 2019/3/12 by  LCD:https://github.com/liucaide .
+
+/***** 模块文档 *****
+ *
+ */
+
+
+
+import Foundation
+import UIKit
+import Value
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+
---
+++ Example/ValuePlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+import Value
+
+
+let arr:[Any] = [3.0,
+                 "3.6",
+                 "",
+                 -9,
+                 [1,"2"],
+                 [1:234,
+                  "23":[1:2,
+                        2:3,
+                        3:4]
+    ]
+]
+
+arr.intValue(1)
+arr.uintValue(1)
+let uu = arr.uint(3) ?? UInt(abs(arr.intValue(3)))
+arr.uintAbsValue(3)
+arr.floatValue(1)
+arr.urlValue(2)
+arr.arrayValue(4)
+arr.stringValue(1).arrayValue(".")
+arr.dictValue(5).dictValue("23")
+"123.88".intValue
+"12a3.88".int ?? 0
+"123.88".floatValue
+"123.88".arrayValue(".")
